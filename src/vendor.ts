8b7dfda04258b4e1370e6394dc62178e5f2db865
frontainer <frontainer@gmail.com>
2017-04-04 08:08:34 +0900

    update: angular 4.0.1, frontpack 0.0.7

--- src/vendor.ts
+++ src/vendor.ts
@@ -4,6 +4,9 @@ import '@angular/common';
 import '@angular/router';
 import '@angular/forms';
 import '@angular/http';
+import '@angular/animations';
 
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
+
+import 'rxjs/Rx';
